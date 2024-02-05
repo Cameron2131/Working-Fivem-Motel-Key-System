340010a53cc8c18d9e779844e36be593d508dd09
Cameron2131 <149907059+Cameron2131@users.noreply.github.com>
2024-02-05 18:27:00 +0000

    Add files via upload

---
+++ amarumods_hotel/QbDoorLock/MotelRoomOne.lua
@@ -0,0 +1,16 @@
+
+
+-- -1244926569 created by CHollings
+Config.DoorList['MotelRoomOne--1244926569'] = {
+    objName = -1244926569,
+    fixText = false,
+    doorLabel = 'Room One',
+    objCoords = vec3(342.338745, 2614.980469, 45.118828),
+    locked = false,
+    distance = 3,
+    items = { ['motelkey_1'] = 1 },
+    doorType = 'door',
+    doorRate = 1.0,
+    pickable = true,
+    objYaw = 29.999996185303,
+}
